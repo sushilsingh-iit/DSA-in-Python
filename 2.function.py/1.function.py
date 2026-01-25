92148d1e3e303b5ba938461a4ec7740860da2797
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-25 23:49:00 +0530

    start variable in function

--- 2.function.py/1.function.py
+++ 2.function.py/1.function.py
@@ -4,3 +4,16 @@ def firstFunction():
     print("hello everyone")
 
 firstFunction()
+
+
+# parameter in function - function defination 
+
+def add(a,b):
+    sum = a + b
+    return sum
+
+print(add(1,2))  # arrgument - calling the function
+
+
+
+
---
+++ 2.function.py/2.variable-function.py
@@ -0,0 +1,7 @@
+s = 202 # globle varibale/ globle scope 
+def funct():
+    s = 202
+    print(s)
+
+funct()
+print(s)
