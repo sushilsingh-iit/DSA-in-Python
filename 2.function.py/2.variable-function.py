f424e1dc00075b6b501423409d91f82ac36fc60c
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-25 23:55:21 +0530

    start diff type of function and variable

--- 2.function.py/2.variable-function.py
+++ 2.function.py/2.variable-function.py
@@ -1,7 +1,9 @@
 s = 202 # globle varibale/ globle scope 
 def funct():
-    s = 202
+    s = 202 # local scope variable
     print(s)
 
 funct()
 print(s)
+
+
---
+++ 2.function.py/3.diff-type-arguments.py
@@ -0,0 +1,7 @@
+# default argument 
+
+def greed(name ,massage):
+    print(name,massage)
+    
+
+greed("sushil" , "hello")
