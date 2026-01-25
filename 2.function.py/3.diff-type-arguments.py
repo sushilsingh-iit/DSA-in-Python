10f718ff9a90f665c7d59d6140c609ac6cb7f1cd
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-26 00:23:56 +0530

    start positational argument

--- 2.function.py/3.diff-type-arguments.py
+++ 2.function.py/3.diff-type-arguments.py
@@ -1,7 +1,26 @@
 # default argument 
 
-def greed(name ,massage):
+def greed(name ,massage="good morning"):  # default arguments 
     print(name,massage)
     
 
 greed("sushil" , "hello")
+greed("sushil")
+# key word arguments
+
+def dree(name ,age ,message):
+    print(message,name,"your age is ",age)
+
+dree(name="suhsil",age=21,message="hello")
+print(dree)
+
+# positional argument 
+def add_number(x,y):
+    print("x " , x)
+    print("y ", y)
+    print(x+y)
+
+add_number(5,6)
+
+
+
