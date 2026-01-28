62dac4c8e23afcd7bae9c0f3a9f1bc43f01ef039
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-28 19:02:49 +0530

    class start

---
+++ 3.oops1/1.class.py
@@ -0,0 +1,19 @@
+# class is define as a blueprint 
+ # class blueprint -- object intance 
+
+class Person: # class is a keyword 
+    pass
+obj = Person() # object 
+print(type(obj))
+print(obj)
+
+class person:
+    name = "suhsil"
+    age = 21 
+    village = "surjanpur"
+
+per = person()
+
+print(per.name)
+print(per.age)
+print(per.village)
