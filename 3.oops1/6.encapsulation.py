60bf3384367068b8f076b7c1650a60a64b50e319
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-30 23:39:45 +0530

    encapsulation half part done

---
+++ 3.oops1/6.encapsulation.py
@@ -0,0 +1,12 @@
+#encapsulation
+# Encapsulation is one of the fundamental concepts in Object-Oriented Programming (OOP).
+# It describes the idea of bundling data (variables) and methods (functions) that work on that data within one unit, like a class.
+
+
+class person :
+    def __init__(self,name ,car):
+        self.__name = name 
+        self.__car = car 
+        
+# second part of encaplsulation
+
