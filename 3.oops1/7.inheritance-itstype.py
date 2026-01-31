97d1a37df5b17937bd914fbef2c3cfb885314d8a
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-31 23:59:00 +0530

    inheritance part

--- 3.oops1/7.inheritance-itstype.py
+++ 3.oops1/7.inheritance-itstype.py
@@ -23,3 +23,23 @@ my_car.start()  # Output: Vehicle started.
 
 # The car can use its own method
 my_car.honk()   # Output: Honk honk!
+
+
+class Animal:
+    def __init__(self, name):
+        self.name = name 
+
+    def eat(self):
+        print(self.name + " is eating.")
+
+class Dog(Animal):
+    def __init__(self, name, breed): 
+        # "super()" is the modern way to call the parent class
+        super().__init__(name)
+        self.breed = breed
+
+# 1. Create the instance (variable is usually lowercase)
+my_dog = Dog("Seru", "Doberman")
+
+# 2. Call the method on the instance
+my_dog.eat()
