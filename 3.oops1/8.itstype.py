cabbbcba7c62844d3cb9e876ab0af01b2f5397e2
Sushil Singh <sushilsingh.iit@gmail.com>
2026-02-01 23:57:48 +0530

    parent and child method

--- 3.oops1/8.itstype.py
+++ 3.oops1/8.itstype.py
@@ -11,12 +11,24 @@
 class parent :
     property = 90 
 
+    def eat(self):
+        print("parent eating")  #
+
 class child(parent):
     property = 99
     
     def Display(self):
         print("child property " , self.property)
         print("parent property ", super().property)
+
+    def eat(self):
+        print("child eating")  #     # method over writing 
+
+
+    def calleat(self):
+        self.eat()   # this is representing is child 
+        super().eat() #this is represent is parent 
+
 
 
 obj = child()
