6033ca70ec6f8ab8fa88a6411094430fdc63853e
Sushil Singh <sushilsingh.iit@gmail.com>
2026-02-04 23:03:27 +0530

    complete polymorphism

--- 4.oops2/2.polymorphism.py
+++ 4.oops2/2.polymorphism.py
@@ -26,3 +26,22 @@ a.eat()
 
 
 # polymorphism is using inheritance 
+
+
+class BirdFly :
+    def flyBird (self,bird):  # i need to have bird object 
+        bird.fly ()
+class Parrot :
+    def fly (self):
+        print("parrot is flying ")
+
+class Crow :
+    def fly(self):
+        print("Crow is flying ")
+
+p = Parrot()
+c = Crow()
+
+bf = BirdFly()
+bf.flyBird(p)
+bf.flyBird(c)
